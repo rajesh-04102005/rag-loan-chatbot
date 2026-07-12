cb715269a60b1ffb06b1e2d618e164fd8cf264bf
Rajesh Giri <rajeshgirikadali@gmail.com>
2026-07-12 20:07:46 +0530

    App.py Updated

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask, request, jsonify, render_template
 from rag_core import ask_rag
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/chatbot")
 def chatbot():
     return render_template("chat_bot.html")
-
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -28,8 +26,6 @@ def chat():
     answer = ask_rag(user_query)
     return jsonify({"reply": answer})
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
